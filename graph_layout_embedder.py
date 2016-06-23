b3745885731277da2999dd1ba3014a95ab2dcd23
Fabrizio Costa <xfcosta@gmail.com>
2016-06-23 15:57:35 +0200

    removed reimplementation

--- graph_layout_embedder.py
+++ graph_layout_embedder.py
@@ -2,7 +2,6 @@
 """Provides layout in 2D of vector instances."""
 
 import random
-import math
 from collections import defaultdict
 
 import networkx as nx
@@ -14,9 +13,7 @@ from matplotlib.collections import PatchCollection
 from matplotlib.patches import Polygon
 
 import numpy as np
-from numpy.linalg import inv
 
-from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics.pairwise import pairwise_kernels
 from sklearn.linear_model import LogisticRegression
@@ -47,159 +44,6 @@ def serialize_dict(the_dict, full=True, offset='small'):
         return '\n'.join(text)
     else:
         return ""
-
-# -----------------------------------------------------------------------------
-
-
-class KKEmbedder(BaseEstimator, ClassifierMixin):
-    """Given a graph computes 2D embedding.
-
-    Based on the algorithm from:
-    Tomihisa Kamada, and Satoru Kawai. "An algorithm for drawing general
-    undirected graphs." Information processing letters 31, no. 1 (1989): 7-15.
-    """
-
-    def __init__(self, stop_eps=1, n_iter=20,
-                 init_pos=None):
-        """Constructor."""
-        self.stop_eps = stop_eps
-        self.n_iter = n_iter
-        self.init_pos = init_pos
-        self.dms = []
-
-    def _compute_all_pairs(self, graph, weight=None, normalize=False):
-        lengths = nx.all_pairs_dijkstra_path_length(graph, weight=weight)
-        max_length = max([max(lengths[i].values()) for i in lengths])
-        if normalize:
-            for i in lengths:
-                for j in lengths[i]:
-                    lengths[i][j] = float(lengths[i][j]) / max_length
-        return lengths
-
-    def _compute_initial_pos(self, graph):
-        _radius = 1
-        _offset = 0
-        n = len(graph)
-        pos = {id: np.array([_radius * math.cos(theta - math.pi / 2) + _offset,
-                             _radius * math.sin(theta - math.pi / 2) + _offset]
-                            )
-               for id, theta in enumerate(
-            np.linspace(0, 2 * math.pi * (1 - 1 / float(n)), num=n))}
-        return pos
-
-    def _compute_dE(self, pos=None, lengths=None, weights=None, m=None):
-        dEx = 0
-        dEy = 0
-        d2Ex2 = 0
-        d2Ey2 = 0
-        d2Exy = 0
-        d2Eyx = 0
-        for i in pos:
-            if i != m:
-                xmi = pos[m][0] - pos[i][0]
-                ymi = pos[m][1] - pos[i][1]
-                xmi2 = xmi * xmi
-                ymi2 = ymi * ymi
-                xmi_ymi2 = xmi2 + ymi2
-                lmi = lengths[m][i]
-                kmi = weights[m][i] / (lmi * lmi)
-                dEx += kmi * (xmi - (lmi * xmi) / math.sqrt(xmi_ymi2))
-                dEy += kmi * (ymi - (lmi * ymi) / math.sqrt(xmi_ymi2))
-                d2Ex2 += kmi * (1 - (lmi * ymi2) / math.pow(xmi_ymi2, 1.5))
-                d2Ey2 += kmi * (1 - (lmi * xmi2) / math.pow(xmi_ymi2, 1.5))
-                res = kmi * (lmi * xmi * ymi) / math.pow(xmi_ymi2, 1.5)
-                d2Exy += res
-                d2Eyx += res
-        return dEx, dEy, d2Ex2, d2Ey2, d2Exy, d2Eyx
-
-    def _compute_dm(self, pos=None, lengths=None, weights=None, m=None):
-        dEx = 0
-        dEy = 0
-        for i in pos:
-            if i != m:
-                xmi = pos[m][0] - pos[i][0]
-                ymi = pos[m][1] - pos[i][1]
-                xmi2 = xmi * xmi
-                ymi2 = ymi * ymi
-                xmi_ymi2 = xmi2 + ymi2
-                lmi = lengths[m][i]
-                kmi = weights[m][i] / (lmi * lmi)
-                dEx += kmi * (xmi - (lmi * xmi) / math.sqrt(xmi_ymi2))
-                dEy += kmi * (ymi - (lmi * ymi) / math.sqrt(xmi_ymi2))
-        return math.sqrt(dEx * dEx + dEy * dEy)
-
-    def _compute_m(self, pos=None, lengths=None, weights=None, id=0):
-        self.dms = np.array([self._compute_dm(pos, lengths, weights, m)
-                             for m in pos])
-        m = np.argsort(-self.dms)[id]
-        return m
-
-    def _compute_dxdy(self, pos=None, lengths=None, weights=None, m=None):
-        dEx, dEy, d2Ex2, d2Ey2, d2Exy, d2Eyx = self._compute_dE(pos,
-                                                                lengths,
-                                                                weights,
-                                                                m)
-        A = np.array([[d2Ex2, d2Exy], [d2Eyx, d2Ey2]])
-        B = np.array([[-dEx], [-dEy]])
-        X = inv(A).dot(B)
-        dx = X[0]
-        dy = X[1]
-        return dx, dy
-
-    def _update(self, pos=None, lengths=None, weights=None):
-        m = self._compute_m(pos, lengths, weights)
-        dx, dy = self._compute_dxdy(pos, lengths, weights, m)
-        pos[m][0] += dx
-        pos[m][1] += dy
-        return m
-
-    def _scale(self, init_pos):
-        _min = -0.5
-        _max = 0.5
-        pos = dict()
-        max_x = max([init_pos[id][0] for id in init_pos])
-        min_x = min([init_pos[id][0] for id in init_pos])
-        max_y = max([init_pos[id][1] for id in init_pos])
-        min_y = min([init_pos[id][1] for id in init_pos])
-        for id in init_pos:
-            x = init_pos[id][0]
-            y = init_pos[id][1]
-            # standardize
-            x = (x - min_x) / (max_x - min_x)
-            y = (y - min_y) / (max_y - min_y)
-            # rescale
-            x = x * (_max - _min) + _min
-            y = y * (_max - _min) + _min
-            pos[id] = np.array([x, y])
-        return pos
-
-    def _compute_weights(self, graph):
-        weights = np.ones((len(graph), len(graph)))
-        for u, v in graph.edges():
-            val = graph.edge[u][v].get('weight', 1)
-            weights[u][v] = val
-        return weights
-
-    def transform(self, graph, normalize=True):
-        """Transform."""
-        lengths = self._compute_all_pairs(graph, weight='len',
-                                          normalize=normalize)
-        weights = self._compute_weights(graph)
-        if self.init_pos is None:
-            pos = self._compute_initial_pos(graph)
-        else:
-            pos = self._scale(self.init_pos)
-        effective_n_iter = self.n_iter * len(graph)
-        for i in range(effective_n_iter):
-            m = self._update(pos, lengths, weights)
-            if i % 100 == 0:
-                logger.debug('iteration %d/%d score:%.2f threshold:%.2f' %
-                             (i, effective_n_iter, self.dms[m], self.stop_eps))
-            if self.dms[m] < self.stop_eps or self.dms[m] != self.dms[m]:
-                logger.debug('Stopped at iteration %d/%d with score %.2f' %
-                             (i, effective_n_iter, self.dms[m]))
-                break
-        return pos
 
 # -----------------------------------------------------------------------------
 
@@ -567,41 +411,6 @@ class Embedder(object):
                 raise Exception('Assigned NaN value to position')
             graph.node[v]['pos'] = layout_pos[v]
 
-    def annotate_graph_with_KK_layout(self,
-                                      graph,
-                                      fast=False,
-                                      stop_eps=1,
-                                      n_iter=20,
-                                      k=1,
-                                      warm_start=True,
-                                      random_state=1):
-        """Annotate graph with layout information."""
-        # remove previous positional annotations
-        for v in graph.nodes():
-            if 'pos' in graph.node[v]:
-                graph.node[v].pop('pos')
-        if warm_start is True:
-            init_pos = nx.graphviz_layout(graph, prog='neato')
-        else:
-            init_pos = None
-        if fast:
-            layout_pos = init_pos
-        else:
-            layout_pos = KKEmbedder(
-                init_pos=init_pos,
-                stop_eps=stop_eps,
-                n_iter=n_iter).transform(graph, normalize=False)
-        # annotate nodes with positional information
-        for v in graph.nodes():
-            # check that no NaN is assigned as a coordinate
-            if(layout_pos[v][0] == layout_pos[v][0] and
-               layout_pos[v][1] == layout_pos[v][1]):
-                pass
-            else:
-                print v, layout_pos[v], graph.neighbors(v)
-                raise Exception('Assigned NaN value to position')
-            graph.node[v]['pos'] = layout_pos[v]
-
     def remove_class_nodes(self, graph):
         """Clean graph."""
         ids = graph.nodes()
@@ -676,7 +485,7 @@ class Embedder(object):
                       file_name=''):
         """Display graph."""
         if target_dict is None:
-                target_dict = {i: i for i in set(self.target)}
+            target_dict = {i: i for i in set(self.target)}
         fig, ax = plt.subplots(figsize=(int(1 * figure_size), figure_size))
         if display_only_class:
             # rebuild the class graph
